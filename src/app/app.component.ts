e92759081603655ac2ead1e8b66fa702e71fa1e2
Jakub Mis <jakub.mis@ardentcode.com>
2023-10-01 13:24:48 +0200

    Dev/Prod mode setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'foodie';
+  mode: boolean;
+
+  ngOnInit(): void {
+    this.mode = environment.production;
+  }
 }
