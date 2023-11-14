fc8ac9f774bfe3c8e34567bd1debd45af834a75a
Jakub Mis <jakub.mis@ardentcode.com>
2023-11-14 17:10:43 +0100

    Leaflet map routing

--- src/app/shared/components/leaflet-map/leaflet-map.component.ts
+++ src/app/shared/components/leaflet-map/leaflet-map.component.ts
@@ -1,26 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { circle, Icon, icon, IconOptions, latLng, marker, Point, tileLayer } from 'leaflet';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { circle, Icon, icon, IconOptions, latLng, marker, Point, tileLayer, Routing, Map, LatLng } from 'leaflet';
 import { LocationService } from '../../../services/location.service';
 import { ObjectType } from '../../../models/utils';
+import 'leaflet-routing-machine';
 
 @Component({
   selector: 'app-leaflet-map',
   templateUrl: './leaflet-map.component.html',
-  styleUrls: ['./leaflet-map.component.scss']
+  styleUrls: ['./leaflet-map.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LeafletMapComponent implements OnInit {
 
   @Input() coords;
 
   options: ObjectType;
+  currentPos: GeolocationCoordinates;
+  hideRoutingAgenda = true;
+  map: Map;
+  zoomValue = 17;
+  distance: number;
 
-  constructor(private locationService: LocationService) {}
+  constructor(private locationService: LocationService,private cdr: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     this.locationService.updateCurrentLocation().subscribe(currentPos => {
+      this.currentPos = currentPos;
       this.options = {
         layers: [
-          tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 20 }),
+          tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 20,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          }),
           circle(latLng(this.coords.lat, this.coords.lon), { radius: 5, color: 'purple' }),
           marker(latLng(this.coords.lat, this.coords.lon), {
             icon: icon({
@@ -28,8 +39,8 @@ export class LeafletMapComponent implements OnInit {
               iconAnchor: new Point(currentPos.latitude - 28, currentPos.longitude + 22),
               iconSize: [50, 50],
               iconUrl: 'assets/markers/purple-marker.svg',
-              shadowUrl: undefined,
-              iconRetinaUrl: 'assets/markers/purple-marker.svg'
+              iconRetinaUrl: 'assets/markers/purple-marker.svg',
+              shadowUrl: undefined
             } as IconOptions)
           }),
           circle(latLng(currentPos.latitude, currentPos.longitude), { radius: 5, color: 'red' }),
@@ -44,10 +55,43 @@ export class LeafletMapComponent implements OnInit {
             } as IconOptions)
           })
         ],
-        zoom: 18,
+        zoom: this.zoomValue,
+        minZoom: 4,
         maxZoom: 20,
         center: latLng(this.coords.lat, this.coords.lon)
       };
+      this.cdr.markForCheck();
     });
+  }
+
+  zoom(zoom = this.zoomValue, center: LatLng): void {
+    this.map.setView(center).setZoom(zoom);
+  }
+
+  zoomDestination(): void {
+    this.zoom(this.zoomValue, latLng(this.coords.lat, this.coords.lon));
+  }
+
+  zoomMe(): void {
+    this.zoom(this.zoomValue, latLng(this.currentPos.latitude, this.currentPos.longitude));
+  }
+
+  onMapReady(map: Map) {
+    this.map = map;
+    const waypoints = [latLng(this.currentPos.latitude, this.currentPos.longitude), latLng(this.coords.lat, this.coords.lon)];
+    Routing.control({
+      waypoints,
+      plan: new Routing.Plan(waypoints, {
+        createMarker: () => false
+      }),
+      useZoomParameter: true,
+      lineOptions: {styles: [{color: '#00750e'}], extendToWaypoints: false, missingRouteTolerance: 0}
+    }).addTo(map);
+
+    this.distance = this.map.distance(
+      latLng(this.currentPos.latitude, this.currentPos.longitude), latLng(this.coords.lat, this.coords.lon)
+    ) / 1000;
+
+    this.cdr.detectChanges();
   }
 }
